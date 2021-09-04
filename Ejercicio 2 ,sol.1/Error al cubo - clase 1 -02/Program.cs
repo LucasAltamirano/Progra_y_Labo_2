c68346a151874cb08ec55e275bcfc5c19dae131f
Lucas javier Altamirano <altamiranolucasjavier@gmail.com>
2021-09-04 12:52:17 -0300

    Ejercicio 2 terminado

---
+++ Ejercicio 2 ,sol.1/Error al cubo - clase 1 -02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace valor_al_Cuadrado_Cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+
+            // dato que levanto y devuelve en string
+
+            double number;
+            do
+            {
+               
+                Console.WriteLine("Ingrese un numero mayor que cero ");
+                if (double.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number > 0)
+                    {
+
+                        Console.WriteLine("NUMERO   INGRESADO {0}", number);
+                        double result2=Math.Pow(number,2);
+                        double result3 = Math.Pow(number, 3);
+                        //int resultado = (int)result;
+                        Console.WriteLine("El valor al cuadrado  es:{0}",result2);
+                        Console.WriteLine("El valor al cubo es:{0}", result3);
+                        flag = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error debe ingresar un valor mayor a cero ");
+                        flag = -1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error  ingresar un valor valido ");
+                    flag = -1;
+                }
+            } while (flag < 0);
+
+
+        }
+    }
+
+    
+
+}
