8bbe5b486dc1dd706e788904953af17961dca0c7
Lucas javier Altamirano <altamiranolucasjavier@gmail.com>
2021-09-04 12:55:11 -0300

    Ejercicio 2 finalizado

---
+++ Consola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Biblioteca;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador s1 = new Sumador(); 
+
+        }
+    }
+}
---
+++ ejercicio 7/Recibo de sueldo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+/*
+ *Consigna#
+
+Escribir un programa que imprima por consola un triángulo como el siguiente:
+El usuario indicará cuál será la altura del triángulo ingresando un número entero positivo.
+Para el ejemplo anterior, la altura ingresada fue de 5.
+ * 
+ */
+namespace Recibo_de_sueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pisos;
+            int pisoActual = 0;
+            bool valido = true;
+            do
+            {
+                Console.WriteLine("Ingrese el numero de pisos del triangulo");
+                if (int.TryParse(Console.ReadLine(), out pisos))
+                {
+                     do
+                     {
+                        for (int i = 0; i < pisos; i++)
+                        {
+                            Console.Write("#");
+                        }
+                       
+
+                        pisoActual++;
+
+                     } while (pisoActual!=pisos);
+                    
+                    valido = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error ,ingrese un numero positivo");
+                    valido = false;
+                }
+
+
+
+
+
+            } while (valido ==false);
+          
+                
+            
+          
+           
+
+        }
+    }
+}
